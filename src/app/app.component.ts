e63d7c8312dea986558e1e630bc96c16113a7a5a
Roberta de Siqueira <designer.roberta@gmail.com>
2023-12-21 10:48:23 -0300

    Iniciando Portfólio

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  templateUrl: './modules/portfolio/pages/home/home.component.html',
 })
 export class AppComponent {
-  title = 'curso-angular-portfolio';
+  title = "Meu Portfólio";
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,14 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
